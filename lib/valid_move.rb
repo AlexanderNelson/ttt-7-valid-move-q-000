19e24af17c8e639e9214aa8d6faf8b014ae0dff3
AlexanderNelson <amvetserv@gmail.com>
2016-03-05 22:16:38 +0000

    nitrous-backup at Sat Mar  5 22:16:38 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
-
-
+def valid_move?(board, position)
+  position.to_i-1
+  if position_taken? == false && board[position] <8
+    return true
+  elseif position_taken? != false || board[position] !<8
+    return false
+  end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  position.to_i-1
+  board[position] != " " && board[position] != "" && board[position] != "nil"  && board[position] == "X" || board[position] =="O"
+  end
+end
